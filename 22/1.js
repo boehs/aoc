ae021a636d0ef3c16dd4a36bc212a6f67a6beb8e
Evan Boehs <evan@boehs.org>
2022-12-01 12:56:14 -0500

    Day 1 pt 2

--- 22/1.js
+++ 22/1.js
@@ -2,7 +2,18 @@
 document.body.textContent
     .split('\n\n')
     .map(item => item.split('\n'))
-    .reduce((a, b) => Math.max(a, b.reduce((p, c) => p + Number(c), 0)), -Infinity);
+    .reduce((a, b) => Math.max(a, b.reduce((p, c) => p + Number(c), 0)), 0);
 
 /// Golfed
 document.body.textContent.split("\n\n").map((e=>e.split("\n"))).reduce(((e,t)=>Math.max(e,t.reduce(((e,t)=>e+ +t),0))),0)
+
+// Top Three
+document.body.textContent
+    .split('\n\n')
+    .map(item => item
+        .split('\n')
+        .map(Number)
+        .reduce((e,t) => e+t))
+    .sort((a, b) => b - a)
+    .slice(0,2)
+    .reduce((e,t) => e+t)
