5088668900ea9dd6470be689a122c31c30cdde78
Evan Boehs <evan@boehs.org>
2022-12-08 00:06:30 -0500

    undefined by default in template

--- template.ts
+++ template.ts
@@ -2,7 +2,7 @@ import { Challenge, Tests } from "~/types"
 
 const sample = ``
 
-export const one: Challenge = (input) => input
+export const one: Challenge = (input) => undefined
 
 export const tests: Tests = [
     [one, sample, ],
