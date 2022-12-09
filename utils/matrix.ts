c1acc4f23e2843679323aa7a64f5ad8cc3b89400
Evan Boehs <evan@boehs.org>
2022-12-09 17:50:27 -0500

    matrix funcs

--- utils/matrix.ts
+++ utils/matrix.ts
@@ -6,3 +6,16 @@ export const transpose = (matrix: any[][]) => matrix[0].map((_, index) => matrix
 
 export const matrixFromCharGrid = (input: string) => input.split('\n').map(line => line.split(''))
 export const matrixFromIGrid = (input: string) => input.split('\n').map(line => line.split('').map(Number))
+
+export const createMatrix = (x: number, y: number, fill: string = ' ') => [...Array(x)].map(() => [...Array(y)].fill(fill))
+export const toString = (matrix: any[][],seperator: string = '') => {
+    const sizes = Array(matrix[0].length).fill(0)
+    return matrix
+        .map(a => a.map((v, i) => {
+            const s = v.toString();
+            if (sizes[i] < s.length) sizes[i] = s.length;
+            return s;
+        }))
+        .map(a => a.map((s, i) => s.padEnd(sizes[i])).join(seperator))
+        .join('\n');
+}
