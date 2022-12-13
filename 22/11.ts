7e1aa021ae159caa1618c1b5f3de3716989df6c0
Evan Boehs <evan@boehs.org>
2022-12-13 01:42:35 -0500

    help (day 11.2)

--- 22/11.ts
+++ 22/11.ts
@@ -28,61 +28,64 @@ Test: divisible by 17
   If true: throw to monkey 0
   If false: throw to monkey 1`
 
-const extractNumber = (input: string) => Number(input.replaceAll(/[^0-9]*/g,''))
+const extractNumber = (input: string) => Number(input.replaceAll(/[^0-9]*/g, ''))
 const parseMonkey = (monkey: string) => {
-    const [idStr, itemsStr, operationStr, testStr, trueStr, falseStr] = monkey.split('\n')
-    
-    const [id, test, ifTrue, ifFalse] = [idStr,testStr,trueStr,falseStr].map(extractNumber)
-    
-    const operation = operationStr.replace('Operation: new = ','')
-    const operationFn = Function('old', `return ${operation}`)
-    
-    const items = itemsStr.replace('Starting items: ', '').split(', ').map(Number)
-    
-    return {
-        id,
-        items,
-        test,
-        ifTrue,
-        ifFalse,
-        operationFn,
-        inspected: 0
-    }
+  const [idStr, itemsStr, operationStr, testStr, trueStr, falseStr] = monkey.split('\n')
+
+  const [id, test, ifTrue, ifFalse] = [idStr, testStr, trueStr, falseStr].map(extractNumber)
+
+  const operation = operationStr.replace('Operation: new = ', '')
+  const operationFn = Function('old', `return ${operation}`)
+
+  const items = itemsStr.replace('Starting items: ', '').split(', ').map(Number)
+
+  return {
+    id,
+    items,
+    test,
+    ifTrue,
+    ifFalse,
+    operationFn,
+    inspected: 0
+  }
 }
 
-const monkeyBusiness = (input,worryManagement,rounds) => {
-    const monkeys = input.split('\n\n').map(parseMonkey)
-    // 20 Rounds
-    for (let i = 0; i < rounds; i++) {
-        monkeys.forEach(monkey => {
-            monkey.items.forEach(item => {
-                // worryManagement is major change between both parts
-                const newWorry = worryManagement
-                    ? Math.floor(monkey.operationFn(item) / 3)
-                    : monkey.operationFn(item)
-                monkeys[newWorry % monkey.test == 0
-                        ? monkey.ifTrue
-                        : monkey.ifFalse].items.push(newWorry)
-                monkey.inspected++
-            })
-            monkey.items = []
-        })
-    }
-    
-    return monkeys
-        // Sort by most passes
-        .sort((a,b) => b.inspected - a.inspected)
-        // Get top two
-        .slice(0,2)
-        // Calculate monkey business!
-        .reduce((a,b) => a*b.inspected,1)
+const monkeyBusiness = (input, worryManagement, rounds) => {
+  const monkeys = input.split('\n\n').map(parseMonkey)
+  // Fucking modular arithmetic. Too many hours googling
+  const M = monkeys.reduce((a, b) => a * b.test, 1)
+
+  // 20 Rounds
+  for (let i = 0; i < rounds; i++) {
+    monkeys.forEach(monkey => {
+      monkey.items.forEach(item => {
+        // worryManagement is major change between both parts
+        const newWorry = worryManagement
+          ? Math.floor(monkey.operationFn(item) / 3)
+          : monkey.operationFn(item)
+        monkeys[newWorry % monkey.test == 0
+          ? monkey.ifTrue
+          : monkey.ifFalse].items.push(newWorry % M)
+        monkey.inspected++
+      })
+      monkey.items = []
+    })
+  }
+
+  return monkeys
+    // Sort by most passes
+    .sort((a, b) => b.inspected - a.inspected)
+    // Get top two
+    .slice(0, 2)
+    // Calculate monkey business!
+    .reduce((a, b) => a * b.inspected, 1)
 }
 
-export const one: Challenge = (input) => monkeyBusiness(input,true,20)
-export const two: Challenge = (input) => monkeyBusiness(input,false,10000)
+export const one: Challenge = (input) => monkeyBusiness(input, true, 20)
+export const two: Challenge = (input) => monkeyBusiness(input, false, 10000)
 
 
 export const tests: Tests = [
-    [one, sample, 10605],
-    [two, sample, 2713310158]
+  [one, sample, 10605],
+  [two, sample, 2713310158]
 ]
