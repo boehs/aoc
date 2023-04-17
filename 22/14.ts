bf60379f27bc70f25e2327ad14d8c06e175587b2
Evan Boehs <evan@boehs.org>
2023-04-17 16:51:26 -0400

    part one

--- 22/14.ts
+++ 22/14.ts
@@ -1,9 +1,23 @@
+import logUpdate from "log-update"
 import { Challenge, Tests } from "~/types"
 import { cord } from "~/utils/constants"
 import { Scene } from "~/utils/scene"
 
 const sample = `498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9`
+
+function simulate(cord: cord = [0,0], scene: Scene<string[]>, queue: Set<string>) {
+    const [x,y] = cord
+    queue.delete(`${x},${y}`)
+    for (let dir of [0,-1,1]) {
+        if (scene[y+1][x+dir] == ' ') {
+            scene[y][x] = ' '
+            scene[y+1][x+dir] = '.'
+            queue.add(`${x+dir},${y+1}`)
+            break
+        } 
+    }
+}
 
 export const one: Challenge = (input) => {
     let clusters = input
@@ -16,10 +30,10 @@ export const one: Challenge = (input) => {
     const maxX = Math.max(...xs)
     const minX = Math.min(...xs)
     const ys = clusters.flat(1).map(v => v[1])
-    const maxY = Math.max(...ys)
+    const maxY = Math.max(...ys);
     const minY = Math.min(...ys)
-    clusters = clusters.map(cluster => cluster.map(([x,y]) => [x-minX+1,y-minY+1]))
-    const scene = Scene.blank(maxX-minX+2,maxY-minY+3,' ')
+    clusters = clusters.map(cluster => cluster.map(([x,y]) => [x-minX+1,y-minY+4]))
+    const scene = Scene.blank<string>(maxX-minX+2,maxY-minY+6,' ')
     clusters.forEach((cluster) => {
         cluster.reduce((a,b) => {
             scene.draw(a,b)
@@ -28,7 +42,21 @@ export const one: Challenge = (input) => {
     })
     const dropper = [500-minX+1,0]
     scene[dropper[1]][dropper[0]] = '+'
-    console.log(scene.toString())
+    let spawn = `${500-minX+1},1`
+    let queue = new Set<string>()
+    queue.add(spawn)
+    let i = 0
+    let done = false
+    while(!done) {
+        if (i % 2 == 0) queue.add(spawn)
+        for (let item of [...queue]) {
+            let cords = item.split(',').map(Number) as cord
+            if (cords[1]+1 == maxY-minY+5) done = true
+            simulate(cords, scene, queue)
+        }
+        i++
+    }
+    return scene.flat(2).filter(t => t=='.').length-[...queue].length
 }
 
 export const tests: Tests = [
