59fe86f3c0554ce0f268bad3c7d48c22dafec9b1
Evan Boehs <evan@boehs.org>
2023-04-18 16:57:24 -0400

    part 2 & cleanup

--- 22/14.ts
+++ 22/14.ts
@@ -1,26 +1,10 @@
 import { Challenge, Tests } from "~/types"
 import { cord } from "~/utils/constants"
-import { Scene } from "~/utils/scene"
 
 const sample = `498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9`
 
-function simulate(cord: cord = [0, 0], scene: Scene<string[]>, queue: Set<string>) {
-    const [x, y] = cord
-    queue.delete(`${x},${y}`)
-    for (let dir of [0, -1, 1]) {
-        if (scene[y + 1][x + dir] == ' ') {
-            scene[y][x] = ' '
-            scene[y + 1][x + dir] = '.'
-            queue.add(`${x + dir},${y + 1}`)
-            break
-        }
-    }
-}
-
 const run = (input: string, part: number) => {
-    // Scene Setup
-
     let clusters = input
         .split('\n')
         .map(cluster => cluster
@@ -28,54 +12,59 @@ const run = (input: string, part: number) => {
             .map(cord => cord.split(',').map(Number) as cord)
         )
     const xs = clusters.flat(1).map(v => v[0])
-    const maxX = Math.max(...xs)
-    const minX = Math.min(...xs)
     const ys = clusters.flat(1).map(v => v[1])
-    const maxY = Math.max(...ys);
-    const minY = Math.min(...ys)
 
-    // pt2 - magic numbers to make it work
-    const width = ((maxY - minY) * 2) + 55
+    let maxX = Math.max(...xs),
+        maxY = Math.max(...ys)
 
-    const range = Math.floor((width - (maxX-minX)) / 2)
-    
-    if (part == 1) clusters = clusters.map(cluster => cluster.map(([x, y]) => [x - minX + 1, y - minY + 4]))
-    else clusters = clusters.map(cluster => cluster.map(([x, y]) => {
-        return [x - minX + range, y - minY + 4]
-    }))
+    if (part == 2) {
+        maxX = 500 + (maxY + 2)
+        maxY += 2;
+    }
 
-    const scene = Scene.blank<string>(part == 2 ? width : maxX - minX + 2, maxY - minY + 6 + (part == 2 ? 1 : 0), ' ')
+    const scene: { [key: number]: { [key: number]: string } } = {}
+
+    for (let x = 0; x <= maxX; x++) {
+        if (!scene[x]) scene[x] = {}
+        if (part == 2) scene[x][maxY] = "#";
+    }
 
     clusters.forEach((cluster) => {
-        cluster.reduce((a, b) => {
-            scene.draw(a, b)
-            return b
+        cluster.reduce((from, to) => {
+            const diff = [to[0] - from[0], to[1] - from[1]]
+            const idx = diff.findIndex(v => v != 0)
+            const n = Math.abs(diff[idx])
+            const sign = Math.sign(diff[idx])
+            Array.from({ length: n + 1 }, (x, i) => {
+                if (idx == 0) return [from[0] + (i * sign), from[1]]
+                else return [from[0], from[1] + (i * sign)]
+            }).map(([x, y]) => scene[x][y] = '#')
+            return to
         })
     })
 
-    if (part == 2) scene[maxY - minY + 6] = scene[maxY - minY + 6].fill('#')
-
-    // Setup Simulation
-    const dropper = [500 - (part == 1 ? minX - 1 : minX - range), 0]
-    scene[dropper[1]][dropper[0]] = '+'
-    let spawn = `${500 - (part == 1 ? minX - 1 : minX - range)},0`
-    let queue = new Set<string>()
-    queue.add(spawn)
-
-    // Simulate
-    let done = false
-    while (!done) {
-        queue.add(spawn)
-        for (let item of [...queue]) {
-            let cords = item.split(',').map(Number) as cord
-            if (part == 1 && cords[1] + 1 == maxY - minY + 5) done = true
-            
-            simulate(cords, scene, queue)
+    let sand = 0
+    while (true) {
+        let x = 500,
+            y = 0
+        while (true) {
+            scene[x][y] = undefined
+            if (scene[x][y + 1] == undefined) y++
+            else if (scene[x - 1][y + 1] == undefined) {
+                x--
+                y++
+            } else if (scene[x + 1][y + 1] == undefined) {
+                x++
+                y++
+            } else {
+                scene[x][y] = "."
+                if (part == 2 && x == 500 && y == 0) return ++sand
+                break
+            }
+            if (part == 1 && y == maxY) return sand
         }
-        if (part == 2 && [...queue].length == 0) done = true
+        sand++;
     }
-    if (part == 2) return scene.flat(2).filter(t => t == '.').length - [...queue].length + 1
-    return scene.flat(2).filter(t => t == '.').length - [...queue].length
 }
 
 export const one: Challenge = (input) => run(input, 1)
